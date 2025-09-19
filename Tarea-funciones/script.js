cf8c319280d78254037f1bf344ab82de8d85f9b5
arindalC <arindal.contreras@itam.mx>
2025-09-19 10:01:04 -0600

    Tarea funciones

---
+++ Tarea-funciones/script.js
@@ -0,0 +1,19 @@
+function multiplicacionClasica(num1,num2){
+    return num1 * num2;
+}
+console.log(multiplicacionClasica(2,5));
+
+const multiplicacionArrow = (num1,num2) => {
+  return num1 * num2;
+};
+console.log(multiplicacionArrow(2, 5)); 
+
+function cuadradoClasica(num) {
+  return num * num;
+}
+console.log(cuadradoClasica(3)); 
+
+const cuadradoArrow = (num) => {
+    return num * num;
+}
+console.log(cuadradoArrow(3));
